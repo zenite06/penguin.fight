1513d95aaf634a7a1c4ca7ff04e183d5ad290efe
manudaros <manudaros@icloud.com>
2026-03-10 11:12:26 -0300

    Carta Dano e Inimigo

--- tarefa1/src/CartaDano.java
+++ tarefa1/src/CartaDano.java
@@ -1,3 +1,21 @@
 public class CartaDano {
-    
+    private String nome;
+    private int custo;
+    private int dano;
+
+    public CartaDano(String nome, int custo, int dano) {
+        this.nome = nome;
+        this.custo = custo;
+        this.dano = dano;
+    }
+
+    public void usar(Heroi heroi, Inimigo inimigo) {
+        if (heroi.getEnergia() >= this.custo) {
+            IO.println(heroi.getName() + " usou " + this.nome + "(" + this.dano + " de dano) em " + inimigo.getName() + "!");
+            inimigo.receberDano(this.dano);
+            heroi.setEnergia(this.custo);
+        }
+        else
+            IO.println("Energia insuficiente!");
+    }
 }
--- tarefa1/src/Inimigo.java
+++ tarefa1/src/Inimigo.java
@@ -2,12 +2,14 @@ public class Inimigo {
     private String nome;
     private int vida;
     private int escudo;
+    private int dano;
     private String capa; // Capa da luta (Imagem dos personagens)
 
     public Inimigo(String nome, int vida, int escudo, String capa) { // Setup do inimigo
         this.nome = nome;
         this.vida = vida;
         this.escudo = escudo;
+        this.dano = dano;
         this.capa = capa;
     }
 
@@ -17,6 +19,17 @@ public class Inimigo {
 
     public String getC() {
         return this.capa;
+    }
+
+    public void receberDano(int dano) {
+        int dano_efetivo = dano - this.escudo;
+        if (dano_efetivo > 0) 
+            this.vida -= dano_efetivo;
+        else
+            dano_efetivo = 0;
+        if (this.escudo > 0) {
+            IO.println(this.nome + " defendeu (" + dano_efetivo + " de dano efetivo)");
+        }
     }
 
     // Implementar os métodos faltantes!
